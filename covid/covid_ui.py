32dc4b6e226ec0589f582358110a7a05899e13b1
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-10-28 09:53:11 -0400

    added a bar chart for newest stats

--- covid/covid_ui.py
+++ covid/covid_ui.py
@@ -1,33 +1,90 @@
 # tkinter library
 from tkinter import *
+#matplot library
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#panda
+from pandas import DataFrame
+#regex
+import re
 import covid_scrape as covids
-# declares UI display window
-window = Tk()
-window.geometry('600x275')
-window.title('COVID-19 World-Wide Statistics')
-window.configure(bg='black')
-cStats = str(covids.find_stats()).split('|')
-frame = Frame(master=window,bg='black')
-wrld = cStats[2].replace(' population:\n','\n')
-stats = Label(master=window,text=f'World-Wide {wrld}',bg='black',fg='white')
-stats.pack()
-Label(master=window,text='Top Ten Countries Affected by COVID-19',bg='black',fg='white').pack()
-frame.pack(padx=10,pady=10)
+#global variables
 a=0
 b=4
-def info(x):
-   num=int(x)
-   stats.configure(text=f'{cStats[num-1]}{cStats[num]}')
+chart={'cat':[],'val':[]}
+window = Tk()
+cStats = str(covids.find_stats()).split('|')
+top,center,bottom = Frame(master=window,bg='black'),Frame(master=window,bg='black'),Frame(master=window,bg='black')
+curChart = FigureCanvasTkAgg(plt.Figure(figsize=(1,1)))
+curChart.get_tk_widget().pack(side=RIGHT)
+#functions
+def sorting(item):
+   num=int(item)
+   chart['cat']=[]
+   chart['val']=[]
+   inf = cStats[num].split('\n  ')
+   inf.pop(0)
+   inf[1],inf[3],inf[6]=inf[1].replace('s ',''),inf[3].replace('s ',''),inf[6].replace('\n','')
+   for i in inf[1:7]:
+      item = i.split(':')
+      item[1]=item[1].replace(',','').replace('+','').replace('N/A','0')
+      x= re.search("[0-9]",item[1])
+      if x==None:
+         item[1]=0
+      chart['cat'].append(item[0])
+      chart['val'].append(int(item[1]))
 
+def info(x):
+   global curChart
+   try:
+      curChart.get_tk_widget().pack_forget()
+   except AttributeError:
+      pass
+   fig = plt.Figure(dpi=100,facecolor='black')
+   bars = FigureCanvasTkAgg(fig,top)
+   ax = fig.add_subplot(111,facecolor='black')
+   num=int(x)
+   if num == 2:
+      wrld = cStats[2].replace(' population:\n','\n')
+      stats.configure(text=f'World-Wide {wrld}')
+   else:
+      stats.configure(text=f'{cStats[num-1]}{cStats[num]}')
+   sorting(num)
+   #building graph
+   df = DataFrame(chart,columns=['cat','val'])
+   df = df.groupby('cat').sum()
+   #new cases
+   df1 = df[0:3]
+   #total cases
+   df2 = df[3:6]
+   df1.plot(xlabel='',ylabel='',kind='bar',legend=False,ax=ax,fontsize=8,rot=0,color='r').tick_params(colors='white')
+   bars.get_tk_widget().pack(side='left')
+   bars.draw()
+   curChart=bars
+
+# UI display window
+window.title('COVID-19 World-Wide Statistics')
+window.configure(bg='black')
+stats = Label(master=top,bg='black',fg='white')
+stats.pack(side='right')
+info(2)
+Label(master=center,text='Top Ten Countries Affected by COVID-19',bg='black',fg='white').pack(side=TOP)
+Button(center,text=f'{cStats[0]}{cStats[1]}',width=14,height=2,command=lambda a=f'{2}':info(2)).pack()
+top.pack()
+center.pack()
+bottom.pack(padx=10,pady=10)
+#buttons
 while a < len(cStats)/18:
    r=0
    while r < 5:
-      block = Button(frame,text=f'{cStats[b-1]}{cStats[b]}',width=14,height=2,command=lambda a=f'{b+1}':info(a))
+      block = Button(bottom,text=f'{cStats[b-1]}{cStats[b]}',width=14,height=2,command=lambda a=f'{b+1}':info(a))
       block.grid(row=a,column=r,padx=5,pady=5)
       r+=1
       if b< len(cStats)-3:
          b+=3
    a+=1
 
-# initializes the window instance
+# initializes the window
 window.mainloop()
