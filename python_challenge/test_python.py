2842633ec3855f7e5e1ed84b7edb7828fd1b53fd
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-10-31 16:08:16 -0400

    adding a few more tests

--- python_challenge/test_python.py
+++ python_challenge/test_python.py
@@ -33,3 +33,27 @@ def test_five(p1,p2,ans):
 @pytest.mark.parametrize('p1,p2,ans',[(5,2,'First argument must be less than second'),(1,4,[1,2,3]),(1,1,[]),(-2,3,[-2,-1,0,1,2])])
 def test_six(p1,p2,ans):
    assert ranges(p1,p2) == ans
+
+@pytest.mark.parametrize('p1,ans',[('SEI Rocks!','!SKCOR IES')])
+def test_seven(p1,ans):
+   assert reverseUpcaseString(p1) == ans
+
+@pytest.mark.parametrize('p1,ans',[('a',''),('abc','b'),('123456789','2345678')])
+def test_eight(p1,ans):
+   assert reverseUpcaseString(p1) == ans
+
+@pytest.mark.parametrize('p1,ans',[('hello',{ h: 1, e: 1, l: 2, o: 1 }),('Today is fantastic!',{ T: 1, o: 1, d: 1, a: 3, y: 1, ' ': 2, i: 2, s: 2, f: 1, n: 1, t: 2, c: 1, '!': 1 })])
+def test_nine(p1,ans):
+   assert reverseUpcaseString(p1) == ans
+
+@pytest.mark.parametrize('p1,p2,p3,ans',[(1234,'*',3,'1234'),(123,'0',5,'00123'),(42,'*',10)])
+def test_ten(p1,ans):
+   assert formatWithPadding(p1,p2,p3) == ans
+
+@pytest.mark.parametrize('p1,ans',[('',True),('A',True),('abc',False),('A nut for a jar of tuna',True)])
+def test_eleven(p1,ans):
+   assert isPalindrome(p1) == ans
+
+@pytest.mark.parametrize('p1,p2,ans',[('abc','abc',0),('a1c','a2c',1),('!!!!','****',4)])
+def test_twelve(p1,ans):
+   assert hammingDistance(p1,p2) == ans
